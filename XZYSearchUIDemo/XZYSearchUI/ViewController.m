76a03277cc812027cb1702fcfd83ce6217003f89
徐梓源 <232652571@qq.com>
2019-03-18 15:54:07 +0800

    更新

--- XZYSearchUIDemo/XZYSearchUI/ViewController.m
+++ XZYSearchUIDemo/XZYSearchUI/ViewController.m
@@ -3,7 +3,7 @@
 //  XZYSearchUI
 //
 //  Created by xuziyuan on 2019/3/11.
-//  Copyright © 2019 中移物联网. All rights reserved.
+//  Copyright © 2019 XiePu Tec Co.,Ltd. All rights reserved.
 //
 
 #import "ViewController.h"
